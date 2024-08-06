5435bff1f5fd7cbebaffcee74775380728715e44
Sami <sami.sofrane@hotmail.fr>
2024-08-06 15:41:49 +0200

    mise en place de la navigation des pages existantes

--- App.js
+++ App.js
@@ -2,9 +2,21 @@ import { StyleSheet, View } from "react-native";
 import ConnectionScreen from "./screens/ConnectionScreen";
 import SignUpScreen from "./screens/SignUpScreen";
 import DatePicker from "./screens/DatePicker";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return <SignUpScreen />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Connection" component={ConnectionScreen} />
+        {/* <Stack.Screen name="SignUp" component={SignUpScreen} /> */}
+        <Stack.Screen name="DatePicker" component={DatePicker} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({
