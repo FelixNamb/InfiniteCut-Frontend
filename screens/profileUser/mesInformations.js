09df5d3349c8f6a0129793dd0f782bf609177684
Ramy <ramycherrouk@gmail.com>
2024-08-07 17:32:32 +0200

    ConceptScreen  mesinformations

---
+++ screens/profileUser/mesInformations.js
@@ -0,0 +1,23 @@
+import { TextInput } from "react-native";
+import Header from "../../components/Header";
+import { TextInput, View } from "react-native";
+//import { useState } from "react";
+export default function mesInformations (){
+    return(
+        <View style={styles.container}>
+        <Header
+          style={styles.header}
+          title=""
+          colorScissors={false}
+          colorUser={false}
+        />
+     <Text style={styles.txt}>Mon compte</Text>
+<Text>Mes informations</Text>
+{/* <TextInput      
+       onChangeText={(value) => (value)}
+
+       value={}
+     /> */}
+   </View>
+    )
+}
